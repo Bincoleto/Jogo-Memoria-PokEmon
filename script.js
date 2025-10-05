904b79c2d7c6be625a19bb84ee702626473317a4
Marcio Bincoleto <marciobincoletojunior@gmail.com>
2025-10-04 23:55:29 -0300

    Feito validação dos pares, fixão da imagem quando o par e igual e voltando para o estado original quando diferente

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function fetchPokemons(){
 const getPokemons = async () => {
     const listaPokemons = []
     
-    while (listaPokemons.length < 7) {
+    while (listaPokemons.length < 9) {
         const date =  await fetchPokemons()
 
         if(date.sprites.other.dream_world.front_default !== null){
@@ -28,6 +28,7 @@ const getPokemons = async () => {
     }
 
     const listaPokemonsDuplicados = [...listaPokemons, ...listaPokemons]
+    listaPokemonsDuplicados.sort(() => Math.random()- 0.5)
 
 
     return listaPokemonsDuplicados
@@ -50,15 +51,54 @@ async function buildCards(){
         div.addEventListener('click', function() {
             // alert(`${pokemon[i].nome}`)
             div.classList.remove('flipped')
-            div.classList.add('card')
+            div.classList.add('card', 'no-click')
             img.src = pokemon[i].img
             div.appendChild(img)
-            cartaVirada.push(pokemon[i].nome)
+            cartaVirada.push({
+                pokemon: pokemon[i].nome,
+                elementoHtml: div,
+                imagem: img
+            })
             console.log(cartaVirada)
+            verificarCard(area)
         })
     }
 }
 
 buildCards()
 
+function verificarCard(area){
+
+    if(cartaVirada.length === 2){
+        area.classList.add('no-click')
+        if(cartaVirada[0].pokemon !== cartaVirada[1].pokemon){
+
+            setTimeout(() => {
+                cartaVirada[0].elementoHtml.classList.remove('card')
+                cartaVirada[0].elementoHtml.classList.add('flipped')
+                //Forma 1 de usar com o removeChild, porem pode gera erro no navegador.
+                cartaVirada[0].elementoHtml.removeChild(cartaVirada[0].imagem)
+                cartaVirada[0].elementoHtml.classList.remove('no-click')
+
+                cartaVirada[1].elementoHtml.classList.remove('card')
+                cartaVirada[1].elementoHtml.classList.add('flipped')
+                // Forma 2 de usar, evita erro no navegador.
+                // Essa forma na minha opinião foi a melhor forma de remover o elemento.
+                cartaVirada[1].imagem.remove()
+                cartaVirada[1].elementoHtml.classList.remove('no-click')
+                area.classList.remove('no-click')
+
+                cartaVirada.length = 0
+                console.log('Esta repetido')
+            }, 1000);
+            
+        }else{
+            cartaVirada.length = 0
+            area.classList.remove('no-click')
+        }
+    }
+
+}
+
 const cartaVirada = []
+
